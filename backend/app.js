91eeb41b2fcd4c8a0223671efd97bf4029e12fda
= <=>
2024-03-01 14:20:18 +0530

    models,signin,signup routes added

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 import express from 'express'
+import { rootRouter } from './routers/index.js';
+import { userRouter } from './routers/user.js';
+import cors from 'cors'
 
 const app=express();
+app.use(cors());
+app.use(express.json());
 
-
+app.use("/api/v1", rootRouter);
+app.use('/user',userRouter);
 
 app.listen(3000,function(){
     console.log('server is up and running ...')
-    
+
 })
---
+++ backend/routers/user.js
@@ -0,0 +1,96 @@
+import express from 'express';
+import zod from 'zod'
+import User from '../models/users'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const userRouter =express.Router();
+
+//zod 
+const signupBody = zod.object({
+    username: zod.string().email(),
+	firstName: zod.string(),
+	lastName: zod.string(),
+	password: zod.string()
+})
+
+//route signup
+router.post("/signup", async (req, res) => {
+    const { success } = signupBody.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const existingUser = await User.findOne({
+        username: req.body.username
+    })
+
+    if (existingUser) {
+        return res.status(411).json({
+            message: "Email already taken/Incorrect inputs"
+        })
+    }
+
+    
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    })
+    const userId = user._id;
+
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        message: "User created successfully",
+        token: token
+    })
+})
+
+
+//zod
+
+const signinBody = zod.object({
+    username: zod.string().email(),
+	password: zod.string()
+})
+
+//signin
+
+router.post("/signin", async (req, res) => {
+    const { success } = signinBody.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if (user) {
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRET);
+  
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+    
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+})
+
+export default router;
